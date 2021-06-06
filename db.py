ad74200371ef500c33ece73e89246caa7c4b0ac5
MohamedGouaouri <im_gouaouri@esi.dz>
2021-06-06 08:09:07 +0100

    db file added

--- db.py
+++ db.py
@@ -2,13 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
+
 def init_db():
-    engine = create_engine('sqlite:////tmp/nursebot.db')
+    engine = create_engine('sqlite:////./nursebot.db')
     Base.metadata.create_all(engine)
     return engine
+
+
 def get_engine():
-    engine = create_engine('sqlite:////tmp/nursebot.db')
+    engine = create_engine('sqlite:////./nursebot.db')
     return engine
+
+
 def db_session(engine):
     Session = sessionmaker(bind=engine)
     return Session()
